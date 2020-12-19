cac7c0e7007f924937868ef0e760184e2300ab17
ramprits <ramprits-sdei@gmail.com>
2020-12-19 20:11:36 +0530

    adding menu

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
+import { useState } from "react";
 import HeadContent from "./HeadContent";
 
 export default function Layout({ children }) {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <>
       <Head>
@@ -12,7 +15,6 @@ export default function Layout({ children }) {
         <script src="js/main.js"></script>
         <title>React Reserve</title>
       </Head>
-
       <div className="container pb-5">
         <nav className="position-relative navbar navbar-expand-lg navbar-light py-3 mb-5">
           <a className="navbar-brand" href="#">
@@ -26,6 +28,7 @@ export default function Layout({ children }) {
             aria-controls="sideMenuHeaders07"
             aria-expanded="false"
             aria-label="Toggle navigation"
+            onClick={() => setToggle(!toggle)}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -64,7 +67,11 @@ export default function Layout({ children }) {
         </nav>
         <main>{children}</main>
       </div>
-      <div className="d-none fixed-top top-0 bottom-0" id="sideMenuHeaders07">
+      {/* d-none */}
+      <div
+        className={`fixed-top top-0 bottom-0 ${toggle ? "d-none" : ""}`}
+        id="sideMenuHeaders07"
+      >
         <div
           className="position-absolute top-0 right-0 bottom-0 left-0 bg-dark"
           style={{ opacity: 0.7 }}
@@ -90,6 +97,7 @@ export default function Layout({ children }) {
                 data-target="#sideMenuHeaders07"
                 aria-controls="sideMenuHeaders07"
                 aria-label="Close"
+                onClick={() => setToggle(!toggle)}
               ></button>
             </div>
             <div>
